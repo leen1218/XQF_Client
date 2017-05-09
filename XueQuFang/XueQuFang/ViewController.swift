d01255dab817efcbd6d92457fda15e153b2455e1
enli <enli@microstrategy.com>
2017-05-09 20:41:49 +0800

    create initial project

---
+++ XueQuFang/XueQuFang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XueQuFang
+//
+//  Created by en li on 2017/5/9.
+//  Copyright © 2017年 xqf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
