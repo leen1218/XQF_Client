420757e4bd980aab97dd08ee14c061cf7afdfeca
enli <enli@microstrategy.com>
2017-05-15 14:55:00 +0800

    搜索UI调整

--- XueQuFang/XueQuFang/UI/SearchMainVC.swift
+++ XueQuFang/XueQuFang/UI/SearchMainVC.swift
@@ -16,17 +16,22 @@ class SearchMainVC : UIViewController, UITableViewDataSource, UITableViewDelegat
 		
 		// 初始化UI
 		self.setupUI()
+		
+		// 初始化数据
+		self.setupModel()
 	}
 	
+	// Search Bar
 	var searchbar:UISearchBar!
-	
-	
+
 	// Search Result TableView
 	var searchResultTV:UITableView!
 	let searchResultItemHeight:Int = 44
 	let searchResultItemMaxCount = 10
+	
 	var searchRecords = [SearchResultItem]()
 	var searchResults = [SearchResultItem]()
+	var useSearchRecord:Bool!
 	
 	func setupUI()
 	{
@@ -59,6 +64,21 @@ class SearchMainVC : UIViewController, UITableViewDataSource, UITableViewDelegat
 		// TODO: Add Map View Here
 	}
 	
+	func setupModel()
+	{
+		// 搜索记录来自于沙盒数据
+		guard let recordsSearchStr = UserDefaults.standard.string(forKey: "searchRecords") else {
+			self.useSearchRecord = true
+			return
+		}
+		let records:[String] = recordsSearchStr.components(separatedBy: ",")
+		for record in records
+		{
+			self.searchRecords.append(SearchResultItem.init(item_name: record))
+			self.useSearchRecord = true
+		}
+	}
+	
 	// 向服务器发送搜索请求
 	func search(searchText:String)
 	{
@@ -73,12 +93,24 @@ class SearchMainVC : UIViewController, UITableViewDataSource, UITableViewDelegat
 	func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
 		self.searchbar.showsCancelButton = true
 		self.searchResultTV.isHidden = false
+		if searchBar.text == ""
+		{
+			self.useSearchRecord = true
+		}
+		else
+		{
+			self.useSearchRecord = false
+		}
+		self.searchResultTV.reloadData()
 	}
 	
 	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
 		if (searchBar.text == "") {
+			self.useSearchRecord = true
+			self.searchResultTV.reloadData()
 			return
 		}
+		self.useSearchRecord = false
 		self.search(searchText: searchText)
 	}
 	
@@ -88,16 +120,16 @@ class SearchMainVC : UIViewController, UITableViewDataSource, UITableViewDelegat
 		}
 		self.search(searchText: searchBar.text!)
 		
-		//
+		// 更新search records
+		self.updateSearchRecords(searchText: searchBar.text!)
+		
+		// 更新UI
 		self.searchbar.resignFirstResponder()
-		self.searchbar.showsCancelButton = false
-		self.searchbar.text = ""
-		self.searchResultTV.isHidden = false
 	}
 	
 	func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
 		self.searchbar.resignFirstResponder()
-		self.searchbar.showsCancelButton = true
+		self.searchbar.showsCancelButton = false
 	}
 	
 	func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
@@ -106,11 +138,41 @@ class SearchMainVC : UIViewController, UITableViewDataSource, UITableViewDelegat
 		self.searchbar.text = ""
 		self.searchResultTV.isHidden = true
 	}
+	
+	func updateSearchRecords(searchText:String)
+	{
+		for i in 0 ..< self.searchRecords.count {
+			if self.searchRecords[i].name == searchText
+			{
+				if i > 0 {
+					swap(&self.searchRecords[0], &self.searchRecords[i])
+				}
+				return
+			}
+		}
+		self.searchRecords.insert(SearchResultItem.init(item_name: searchText), at: 0)
+		if (self.searchRecords.count > 10) {
+			self.searchRecords.removeLast()
+		}
+		var searchRecordsStr:String = self.searchRecords[0].name
+		for i in 1 ..< self.searchRecords.count
+		{
+			searchRecordsStr += "," + self.searchRecords[i].name
+		}
+		UserDefaults.standard.set(searchRecordsStr, forKey: "searchRecords")
+	}
 
 	
 // Search Result TableView Delegate
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return self.searchResults.count
+		if (self.useSearchRecord)
+		{
+			return self.searchRecords.count
+		}
+		else
+		{
+			return self.searchResults.count
+		}
 	}
 	func numberOfSections(in tableView: UITableView) -> Int {
 		return 1
@@ -125,8 +187,25 @@ class SearchMainVC : UIViewController, UITableViewDataSource, UITableViewDelegat
 			cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: cellId)
 		}
 
-		cell!.textLabel?.text = self.searchResults[indexPath.row].name
+		cell!.textLabel?.text = self.useSearchRecord! ? self.searchRecords[indexPath.row].name : self.searchResults[indexPath.row].name
 		return cell!
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		// TODO : Go Server for selected item info
+		let selectedItem = self.useSearchRecord! ? self.searchRecords[indexPath.row].name : self.searchResults[indexPath.row].name
+		print("selected item : " + selectedItem!)
+		
+		// 添加search records
+		self.updateSearchRecords(searchText: selectedItem!)
+		
+		// 更新UI
+		self.searchbar.resignFirstResponder()
+		self.searchbar.showsCancelButton = false
+		self.searchbar.text = ""
+		self.searchResultTV.isHidden = true
+		
+		self.useSearchRecord = true
 	}
 	
 // Search Request Delegate
